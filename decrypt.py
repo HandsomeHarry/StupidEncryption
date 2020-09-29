8988cb20cc2251525e21698d2779be032e9d2524
HarryYu31 <harryyu2002@gmail.com>
2020-09-29 19:50:03 +0800

    A little help on decrypting

--- decrypt.py
+++ decrypt.py
@@ -1,34 +1,28 @@
-inFileName = input("Enter filename for encryption: ")
-f = open(inFileName+".txt", "r")
+inFileName = input("Enter filename for decryption (ends in .encrypted): ")
+f = open(inFileName+".encrypted", "r")
 file = f.read()
-encryptedFile = ""
+decryptedFile = ""
 outFileName = input("Enter output filename: ")
-g = open(outFileName+".encrypted", "w+")
+g = open(outFileName+".txt", "w+")
 tmp = 0
 
 # ASCII Encryption
 for i in range(len(file)):
-    if ord(file[i])<=63 and ord(file[i])>=32:
+    if ord(file[i])<=126 and ord(file[i])>=95:
         # 32~63: +63, becomes 95~126 (31)
-        g.write(chr(ord(file[i]) +63))
-    elif ord(file[i])<=79 and ord(file[i])>=64:
+        g.write(chr(ord(file[i]) - 63))
+    elif ord(file[i])<=94 and ord(file[i])>=79:
         # 64~79: +16, becomes 79~94 (15)
-        g.write(chr(ord(file[i]) -15))
-    elif ord(file[i])<=100 and ord(file[i])>=80:
+        g.write(chr(ord(file[i]) - 16))
+    elif ord(file[i])<=78 and ord(file[i])>=58:
         # 80~100: -22, becomes 58~78 (20)
-        g.write(chr(ord(file[i]) +26))
-    elif ord(file[i])<=126 and ord(file[i])>=101:
+        g.write(chr(ord(file[i]) + 22))
+    elif ord(file[i])<=57 and ord(file[i])>=32:
         # 101~126: -69, becomes 32~57 (25)
-        g.write(chr(ord(file[i]) -53))
+        g.write(chr(ord(file[i]) + 69))
     else:
-        # other crap
+        # other crap (unchanged)
         g.write(file[i])
-print("First stage of encryption completed")
 
-# head-to-foot
-goldilock = g.read()
-for i in range(len(goldilock)):
-    g.write(goldilock[len(goldilock)-i-1])
-
-g.write("Get out! You shouldn't be here!")
+print("Decryption completed")
 g.close()
--- encrypt.py
+++ encrypt.py
@@ -1,4 +1,4 @@
-inFileName = input("Enter filename for encryption: ")
+inFileName = input("Enter filename for encryption (ends in .txt): ")
 f = open(inFileName+".txt", "r")
 file = f.read()
 encryptedFile = ""
@@ -10,25 +10,20 @@ tmp = 0
 for i in range(len(file)):
     if ord(file[i])<=63 and ord(file[i])>=32:
         # 32~63: +63, becomes 95~126 (31)
-        g.write(chr(ord(file[i]) +63))
+        g.write(chr(ord(file[i]) + 63))
     elif ord(file[i])<=79 and ord(file[i])>=64:
         # 64~79: +16, becomes 79~94 (15)
-        g.write(chr(ord(file[i]) -15))
+        g.write(chr(ord(file[i]) + 16))
     elif ord(file[i])<=100 and ord(file[i])>=80:
         # 80~100: -22, becomes 58~78 (20)
-        g.write(chr(ord(file[i]) +26))
+        g.write(chr(ord(file[i]) - 22))
     elif ord(file[i])<=126 and ord(file[i])>=101:
         # 101~126: -69, becomes 32~57 (25)
-        g.write(chr(ord(file[i]) -53))
+        g.write(chr(ord(file[i]) - 69))
     else:
-        # other crap
+        # other crap (unchanged)
         g.write(file[i])
-print("First stage of encryption completed")
-
-# head-to-foot
-goldilock = g.read()
-for i in range(len(goldilock)):
-    g.write(goldilock[len(goldilock)-i-1])
+print("Encryption completed")
 
 g.write("Get out! You shouldn't be here!")
 g.close()
