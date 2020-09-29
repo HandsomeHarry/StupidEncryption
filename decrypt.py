840608c83a35a7ec5c6b54d9d90b4c7034cb16d3
HarryYu31 <harryyu2002@gmail.com>
2020-09-30 00:09:52 +0800

    asda

--- decrypt.py
+++ decrypt.py
@@ -1,28 +1,33 @@
 inFileName = input("Enter filename for decryption (.encrypted file): ")
 f = open(inFileName+".encrypted", "r")
-file = f.read()
+Egg = f.read()
+NoEgg = Egg.replace("Get out! You shouldn't be here!", '')
+ff = open(inFileName+".encrypted", "w")
+ff.write(NoEgg)
+ff.flush()
+fff = open(inFileName+".encrypted", "r")
+file = fff.read()
 decryptedFile = ""
 outFileName = input("Enter output filename: ")
 g = open(outFileName+".txt", "w+")
 
 # ASCII Decryption
-def decrypt(h):
-    for i in range(len(file)):
-        if ord(file[i])<=126 and ord(file[i])>=95:
-            # 32~63: +63, becomes 95~126 (31)
-            h.write(chr(ord(file[i]) - 63))
-        elif ord(file[i])<=94 and ord(file[i])>=79:
-            # 64~79: +15, becomes 79~94 (15)
-            h.write(chr(ord(file[i]) - 15))
-        elif ord(file[i])<=78 and ord(file[i])>=58:
-            # 80~100: -22, becomes 58~78 (20)
-            h.write(chr(ord(file[i]) + 22))
-        elif ord(file[i])<=57 and ord(file[i])>=32:
-            # 101~126: -69, becomes 32~57 (25)
-            h.write(chr(ord(file[i]) + 69))
-        else:
-            # other crap (unchanged)
-            h.write(file[i])
+for i in range(len(file)):
+    if ord(file[i])<=126 and ord(file[i])>=95:
+        # 32~63: +63, becomes 95~126 (31)
+        g.write(chr(ord(file[i]) - 63))
+    elif ord(file[i])<=94 and ord(file[i])>=79:
+        # 64~79: +15, becomes 79~94 (15)
+        g.write(chr(ord(file[i]) - 15))
+    elif ord(file[i])<=78 and ord(file[i])>=58:
+        # 80~100: -22, becomes 58~78 (20)
+        g.write(chr(ord(file[i]) + 22))
+    elif ord(file[i])<=57 and ord(file[i])>=32:
+        # 101~126: -69, becomes 32~57 (25)
+        g.write(chr(ord(file[i]) + 69))
+    else:
+        # other crap (unchanged)
+        g.write(file[i])
 
 decrypt(h)
 print("Decryption completed, file", outFileName + ".txt", "has been created!")
--- encrypt.py
+++ encrypt.py
@@ -6,23 +6,22 @@ outFileName = input("Enter output filename: ")
 g = open(outFileName+".encrypted", "w+")
 
 # ASCII Encryption
-def encrypt(h):
-    for i in range(len(file)):
-        if ord(file[i])<=63 and ord(file[i])>=32:
-            # 32~63: +63, becomes 95~126 (31)
-            h.write(chr(ord(file[i]) + 63))
-        elif ord(file[i])<=79 and ord(file[i])>=64:
-            # 64~79: +15, becomes 79~94 (15)
-            h.write(chr(ord(file[i]) + 15))
-        elif ord(file[i])<=100 and ord(file[i])>=80:
-            # 80~100: -22, becomes 58~78 (20)
-            h.write(chr(ord(file[i]) - 22))
-        elif ord(file[i])<=126 and ord(file[i])>=101:
-            # 101~126: -69, becomes 32~57 (25)
-            h.write(chr(ord(file[i]) - 69))
-        else:
-            # other crap (unchanged)
-            h.write(file[i])
+for i in range(len(file)):
+    if ord(file[i])<=63 and ord(file[i])>=32:
+        # 32~63: +63, becomes 95~126 (31)
+        g.write(chr(ord(file[i]) + 63))
+    elif ord(file[i])<=79 and ord(file[i])>=64:
+        # 64~79: +15, becomes 79~94 (15)
+        g.write(chr(ord(file[i]) + 15))
+    elif ord(file[i])<=100 and ord(file[i])>=80:
+        # 80~100: -22, becomes 58~78 (20)
+        g.write(chr(ord(file[i]) - 22))
+    elif ord(file[i])<=126 and ord(file[i])>=101:
+        # 101~126: -69, becomes 32~57 (25)
+        g.write(chr(ord(file[i]) - 69))
+    else:
+        # other crap (unchanged)
+        g.write(file[i])
 
 encrypt(g)
 print("Encryption completed, file", outFileName + ".encrypted", "has been created!")
