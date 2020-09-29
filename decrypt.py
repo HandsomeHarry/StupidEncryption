48215b0aa89d39c79983721b11ea1cec6677af0e
HarryYu31 <harryyu2002@gmail.com>
2020-09-30 01:20:18 +0800

    shorter variable names

--- decrypt.py
+++ decrypt.py
@@ -1,27 +1,27 @@
 inFileName = input("Enter filename for decryption (.encrypted file): ")
 f = open(inFileName+".encrypted", "r").read().replace("Get out! You shouldn't be here!", '')
 open(inFileName+".encrypted", "w").write(f)
-file = open(inFileName+".encrypted", "r").read()
+f = open(inFileName+".encrypted", "r").read()
 outFileName = input("Enter output filename: ")
 g = open(outFileName+".txt", "w+")
 
 # ASCII Decryption
-for i in range(len(file)):
-    if ord(file[i])<=126 and ord(file[i])>=95:
+for i in range(len(f)):
+    if ord(f[i])<=126 and ord(f[i])>=95:
         # 32~63: +63, becomes 95~126 (31)
-        g.write(chr(ord(file[i]) - 63))
-    elif ord(file[i])<=94 and ord(file[i])>=79:
+        g.write(chr(ord(f[i]) - 63))
+    elif ord(f[i])<=94 and ord(f[i])>=79:
         # 64~79: +15, becomes 79~94 (15)
-        g.write(chr(ord(file[i]) - 15))
-    elif ord(file[i])<=78 and ord(file[i])>=58:
+        g.write(chr(ord(f[i]) - 15))
+    elif ord(f[i])<=78 and ord(f[i])>=58:
         # 80~100: -22, becomes 58~78 (20)
-        g.write(chr(ord(file[i]) + 22))
-    elif ord(file[i])<=57 and ord(file[i])>=32:
+        g.write(chr(ord(f[i]) + 22))
+    elif ord(f[i])<=57 and ord(f[i])>=32:
         # 101~126: -69, becomes 32~57 (25)
-        g.write(chr(ord(file[i]) + 69))
+        g.write(chr(ord(f[i]) + 69))
     else:
         # other crap (unchanged)
-        g.write(file[i])
+        g.write(f[i])
 
 g.close()
 print("Decryption completed, file", outFileName + ".txt", "has been created!")
--- encrypt.py
+++ encrypt.py
@@ -1,24 +1,24 @@
-file = open(input("Enter filename for encryption (.txt file): ")+".txt", "r").read()
+f = open(input("Enter filename for encryption (.txt file): ")+".txt", "r").read()
 outFileName = input("Enter output filename: ")
 g = open(outFileName+".encrypted", "w+")
 
 # ASCII Encryption
-for i in range(len(file)):
-    if ord(file[i])<=63 and ord(file[i])>=32:
+for i in range(len(f)):
+    if ord(f[i])<=63 and ord(f[i])>=32:
         # 32~63: +63, becomes 95~126 (31)
-        g.write(chr(ord(file[i]) + 63))
-    elif ord(file[i])<=79 and ord(file[i])>=64:
+        g.write(chr(ord(f[i]) + 63))
+    elif ord(f[i])<=79 and ord(f[i])>=64:
         # 64~79: +15, becomes 79~94 (15)
-        g.write(chr(ord(file[i]) + 15))
-    elif ord(file[i])<=100 and ord(file[i])>=80:
+        g.write(chr(ord(f[i]) + 15))
+    elif ord(f[i])<=100 and ord(f[i])>=80:
         # 80~100: -22, becomes 58~78 (20)
-        g.write(chr(ord(file[i]) - 22))
-    elif ord(file[i])<=126 and ord(file[i])>=101:
+        g.write(chr(ord(f[i]) - 22))
+    elif ord(f[i])<=126 and ord(f[i])>=101:
         # 101~126: -69, becomes 32~57 (25)
-        g.write(chr(ord(file[i]) - 69))
+        g.write(chr(ord(f[i]) - 69))
     else:
         # other crap (unchanged)
-        g.write(file[i])
+        g.write(f[i])
 
 g.write("Get out! You shouldn't be here!")
 g.close()
