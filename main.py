288e891cc02e846d9131d35fb0cb9c869c98bbe6
HarryYu31 <harryyu2002@gmail.com>
2020-09-29 15:26:30 +0800

    update

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
-fileName = input("Enter file name for encryption: ")
-f = open(fileName, "r")
+inFileName = input("Enter filename for encryption: ")
+f = open(inFileName+".txt", "r")
 file = f.read()
 encryptedFile = ""
-g = open("out.txt", "w")
+outFileName = input("Enter output filename: ")
+g = open(outFileName+".txt", "w")
 tmp = 0
-# head-to-foot
+# ASCII-32, ASCII+62
 for i in range(len(file)):
-    g.write(file[len(file)-i-1])
+    if ord(file[i])<=126 and ord(file[i])>=64:
+        g.write(chr(ord(file[i]) - 32))
+    elif ord(file[i])<=63 and ord(file[i])>=32:
+        g.write(chr(ord(file[i]) + 62))
+    else:
+        g.write(file[i])
 print("First stage of encryption completed")
-# ASCII-20
+# head-to-foot
 h = g
 for i in range(len(file)):
     h.write(file[len(file)-i-1])
+print("Second stage of encryption completed")
+# Save file
