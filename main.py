5eb5926b4d8af5cf93866cfaf0c5cef528f92e6d
HarryYu31 <harryyu2002@gmail.com>
2020-09-29 16:06:23 +0800

    asdf

--- main.py
+++ main.py
@@ -3,20 +3,33 @@ f = open(inFileName+".txt", "r")
 file = f.read()
 encryptedFile = ""
 outFileName = input("Enter output filename: ")
-g = open(outFileName+".txt", "w")
+g = open(outFileName+".txt", "w+")
+goldilock = g.read()
 tmp = 0
-# ASCII-32, ASCII+62
+
+# ASCII Encryption
 for i in range(len(file)):
-    if ord(file[i])<=126 and ord(file[i])>=64:
-        g.write(chr(ord(file[i]) - 32))
-    elif ord(file[i])<=63 and ord(file[i])>=32:
-        g.write(chr(ord(file[i]) + 62))
+    if ord(file[i])<=63 and ord(file[i])>=32:
+        # 32~63: +74
+        g.write(chr(ord(file[i]) +74))
+    elif ord(file[i])<=79 and ord(file[i])>=64:
+        # 64~79: -15
+        g.write(chr(ord(file[i]) -15))
+    elif ord(file[i])<=100 and ord(file[i])>=80:
+        # 80~100: +26
+        g.write(chr(ord(file[i]) +26))
+    elif ord(file[i])<=126 and ord(file[i])>=101:
+        # 101~126: -53
+        g.write(chr(ord(file[i]) -53))
     else:
+        # other crap
         g.write(file[i])
+g.write("Get out! You shouldn't be here!")
 print("First stage of encryption completed")
+
 # head-to-foot
-h = g
 for i in range(len(file)):
-    h.write(file[len(file)-i-1])
+    g.write(goldilock[len(file)-i-1])
 print("Second stage of encryption completed")
-# Save file
+
+g.close()
