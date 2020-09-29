e6f2b6e76539519cb3485d59eeb7d5de1076dbed
HarryYu31 <harryyu2002@gmail.com>
2020-09-29 14:52:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ f = open(fileName, "r")
 file = f.read()
 encryptedFile = ""
 g = open("out.txt", "w")
+tmp = 0
+# head-to-foot
 for i in range(len(file)):
     g.write(file[len(file)-i-1])
-print("Write success!")
+print("First stage of encryption completed")
+# ASCII-20
+h = g
+for i in range(len(file)):
+    h.write(file[len(file)-i-1])
